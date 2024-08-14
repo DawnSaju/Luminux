c79ad2376b106006bd1ce8f0ca55958e494d5012
Dawn Saju <dawnsaju2006@gmail.com>
2024-08-15 03:35:26 +0530

    v1

--- src/app/api/auth/[...nextauth].ts
+++ src/app/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import { prisma } from '../../../../lib/prisma';
+import { prisma } from '../../../lib/prisma';
 import { NextAuthOptions } from 'next-auth';
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
