1d6da062e73bc995969f3756129b30bc3b7ff6fa
DawnSaju <70035674+DawnSaju@users.noreply.github.com>
2024-08-17 23:14:18 +0530

    v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,25 @@ import Features from "@/components/Features";
 import Footer from "@/components/Footer";
 import Spline from '@splinetool/react-spline/next';
 
-
 export default function Home() {
   return (
-    <main>
-      <Navbar />
-      <Hero />
-      <Features />
-      <Process />
-      <Team />
-      <Footer />
-      
-    </main>
+    <html>
+      <head>
+        <title>Luminux</title>
+        <meta name="description" content="A comprehensive health support chatbot providing reliable information and guidance for a healthy life." />
+        <meta name="keywords" content="healthcare, AI, chatbot, luminux, Luminux, dawnsaju, headstarter, sivaibala, abhishekshresta" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>
+        <main>
+          <Navbar />
+          <Hero />
+          <Features />
+          <Process />
+          <Team />
+          <Footer />
+        </main>
+      </body>
+    </html>
   );
 }
